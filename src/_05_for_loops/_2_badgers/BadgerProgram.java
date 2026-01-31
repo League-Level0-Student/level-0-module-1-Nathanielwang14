d73d667223af21129b8e8a0385f33a25ded02227
league <league@192.168.1.135>
2026-01-30 17:01:25 -0800

    123

---
+++ src/_05_for_loops/_2_badgers/BadgerProgram.java
@@ -0,0 +1,30 @@
+package _05_for_loops._2_badgers;
+
+public class BadgerProgram {
+	public static void main(String[] args) {
+		
+		int lyrics = 0;
+		int lyrics2 = 0;
+		int snake = 0;
+		
+		for ( lyrics=0; lyrics <= 12; lyrics +=1 ) {
+			System.out.print("Badgers");
+		}
+		
+		System.out.println();
+		
+		for ( lyrics2=1; lyrics2 <= 2; lyrics2 +=1) {
+			System.out.print("Mushroom");
+		}
+		
+		
+		
+		
+		for ( snake=1; snake <= 1; snake +=1) {
+			System.out.println();
+			System.out.print("A Snake!!!!");
+		}
+	
+	}
+
+}
